331ebf4c857641a59e750a50dc9f3a1ffeae1915
b-price <bprice12@students.towson.edu>
2024-07-17 14:56:01 -0400

    Added form validation

--- client/src/components/TeaInputForm.js
+++ client/src/components/TeaInputForm.js
@@ -40,8 +40,9 @@ export default function TeaInputForm({
     let buttonText = isEdit? "Edit Tea" : "Add Tea"
     function onSubmit(event) {
         event.preventDefault()
-        if (event.currentTarget.checkValidity() === false)
+        if (event.currentTarget.checkValidity() === false){
             event.stopPropagation()
+        }
         else {
             setValidated(true)
             try {
@@ -71,8 +72,9 @@ export default function TeaInputForm({
                         id="name"
                         onChange={handleChange}
                         value={isEdit? form.name: undefined}
+                        isInvalid={form.name === undefined || form.name === ""}
                     />
-                    <Form.Control.Feedback type="invalid">Name is required</Form.Control.Feedback>
+                    <Form.Control.Feedback type="invalid">Name is required.</Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group as={Col}>
                     <Form.Label>Tea Type</Form.Label>
@@ -91,14 +93,16 @@ export default function TeaInputForm({
             <Row className="mb-3">
                 <Form.Group as={Col}>
                     <Form.Label>Quantity</Form.Label>
-                    <InputGroup>
+                    <InputGroup hasValidation>
                         <Form.Control
                             required
                             id={"quantity"}
                             onChange={handleChange}
                             defaultValue={isEdit? form.quantity: undefined}
+                            isInvalid={form.quantity === 0 || isNaN(form.quantity)}
                         />
                         <InputGroup.Text>grams</InputGroup.Text>
+                        <Form.Control.Feedback type="invalid">Quantity must be a number > 0.</Form.Control.Feedback>
                     </InputGroup>
                 </Form.Group>
                 <Form.Group as={Col}>
@@ -113,14 +117,16 @@ export default function TeaInputForm({
             <Row className="mb-3">
                 <Form.Group as={Col}>
                     <Form.Label>Cost Per Gram</Form.Label>
-                    <InputGroup>
+                    <InputGroup hasValidation>
                         <InputGroup.Text>$</InputGroup.Text>
                         <Form.Control
                             id={"cost"}
                             onChange={handleChange}
                             defaultValue={isEdit? form.cost: undefined}
+                            isInvalid={isNaN(form.cost)}
                         />
                         <InputGroup.Text>/ gram</InputGroup.Text>
+                        <Form.Control.Feedback type="invalid">Cost must be a number.</Form.Control.Feedback>
                     </InputGroup>
 
                 </Form.Group>
@@ -130,7 +136,9 @@ export default function TeaInputForm({
                         id={"year"}
                         onChange={handleChange}
                         defaultValue={isEdit? form.year: undefined}
+                        isInvalid={isNaN(form.year)}
                     />
+                    <Form.Control.Feedback type="invalid">Year must be a number.</Form.Control.Feedback>
                 </Form.Group>
             </Row>
             <Row className="mb-3">
@@ -140,17 +148,21 @@ export default function TeaInputForm({
                         id={"rating"}
                         onChange={handleChange}
                         defaultValue={isEdit? form.rating: undefined}
+                        isInvalid={isNaN(form.rating) || form.rating > 10 || form.rating < 0 || (form.rating > 0 && form.rating < 1)}
                     />
+                    <Form.Control.Feedback type="invalid">Rating must be a number 1-10.</Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group as={Col} >
                     <Form.Label>Tea to Water Ratio</Form.Label>
-                    <InputGroup>
+                    <InputGroup hasValidation>
                         <Form.Control
                             id={"ratio"}
                             onChange={handleChange}
                             defaultValue={isEdit? form.ratio: undefined}
+                            isInvalid={isNaN(form.ratio)}
                         />
                         <InputGroup.Text>g/mL</InputGroup.Text>
+                        <Form.Control.Feedback type="invalid">Ratio must be a number.</Form.Control.Feedback>
                     </InputGroup>
                 </Form.Group>
             </Row>
